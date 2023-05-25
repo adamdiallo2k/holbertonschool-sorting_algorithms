e1bbf9253903a3fe415619c54c5cc9ff921ea442
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2023-05-26 01:03:57 +0200

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,12 +2,22 @@
 #include <stdlib.h>
 #include "sort.h"
 
+/**
+ * swap - Swaps two integers.
+ * @xp: Pointer to the first integer.
+ * @yp: Pointer to the second integer.
+ */
 void swap(int *xp, int *yp) {
     int temp = *xp;
     *xp = *yp;
     *yp = temp;
 }
 
+/**
+ * print_array - Prints the elements of an array.
+ * @array: The array to be printed.
+ * @size: The size of the array.
+ */
 void print_array(int *array, size_t size) {
     for (size_t i = 0; i < size; i++) {
         printf("%d ", array[i]);
@@ -15,6 +25,11 @@ void print_array(int *array, size_t size) {
     printf("\n");
 }
 
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using the Bubble sort algorithm.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ */
 void bubble_sort(int *array, size_t size) {
     for (size_t i = 0; i < size-1; i++) {
         for (size_t j = 0; j < size-i-1; j++) {
